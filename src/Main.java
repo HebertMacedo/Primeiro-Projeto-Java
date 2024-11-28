7a3627ae736aaef172486e87da91a46e64f77b57
Hebert Rodrigues <hebert.macedo2@gmail.com>
2024-11-28 16:46:03 -0300

    Calcula média

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,20 @@ public class Main {
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
 
+        //Média calculada pelas 3 notas
+        double media = (9.8 + 6.4 + 10) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+
+
 
     }
 }
